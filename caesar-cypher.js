23d7be9ee650f45fc19a57f85c58127cdfea2055
mtmazzo <74214745+mtmazzo@users.noreply.github.com>
2021-08-30 20:28:37 -0400

    Update caesar-cypher.js

---
+++ caesar-cypher.js
@@ -0,0 +1,16 @@
+function encrypt(inputString, key) {
+    var result = "";
+    for(var i = 0; i < inputString.length; i++) {
+      var char = inputString[i];
+      if(char.match(/[a-z]/gi)) {
+        var code = inputString.charCodeAt(i);
+        if(code >= 65 && code <= 90) char = String.fromCharCode(((code - 65 + key) % 26) + 65);
+        else if(code >= 97 && code <= 122) char = String.fromCharCode(((code - 97 + key) % 26) + 97);
+      }
+      result += char;
+    }
+    return result;
+}
+
+//Demonstrate
+console.log(encrypt("WXYZwxyz", 2));
